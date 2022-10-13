63b0758362d5b19be4f6007d4989a75482f52750
vikryb22 <viktoriia.rybitski@yh.nackademin.se>
2022-10-13 10:11:32 +0200

    new commit

--- vikryb-rps/src/games/Main.java
+++ vikryb-rps/src/games/Main.java
@@ -3,7 +3,7 @@ package games;
 public class Main {
     public static void main(String[] args) {
 
-        RockPaperScissors game=new RockPaperScissorsGame("Marcus", 0);
+        RockPaperScissors game=new RockPaperScissors game();
         game.playGame();
     }
 }
